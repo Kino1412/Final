b728ebb8ffa88a44cc6818d33f3e1e55211157c1
Kino1412 <nhoccan1412@gmail.com>
2024-01-15 13:41:45 +0900

    vnnbv

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,7 +1,7 @@
 
 
 <?php
-const SERVER = 'mysql215.phy.lolipop.lan';
+const SERVER = 'mysql220.phy.lolipop.lan';
 const DBNAME = 'LAA1517462-final';
 const USER = 'LAA1517462';
 const PASS = 'Pass1227';
