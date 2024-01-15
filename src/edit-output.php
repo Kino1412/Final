fc36084ed101f2d8118088638b8ba7097a553f8e
Kino1412 <nhoccan1412@gmail.com>
2024-01-15 15:01:05 +0900

    vnm

--- src/edit-output.php
+++ src/edit-output.php
@@ -3,45 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>商品情報変更</title>
+    <title>情報変更</title>
     <link rel="stylesheet" href="css/detail.css">
 </head>
 <body>
-    <?php require 'header.php';?>
     <?php require 'db-connect.php';?>
     <div class="container">
     <?php
 $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('update Shohin set  name=?, ex=?, price=?, size=?, color=?, category=?, sozai=?, zaiko=?, image=? where id=?');
+    $sql=$pdo->prepare('update mylist set name=?, ex=?, category=?, status=?, read_pro=?, author=?, artist=?, image=? where id=?');
     
     if (empty($_POST['name'])) {
-        echo '商品名を入力してください。';
+        echo '漫画名を入力してください。';
     } else
     if (empty($_POST['ex'])) {
-        echo '商品説明を入力してください。';
-    } else
-    if (!preg_match('/[0-9]+/', $_POST['price'])) {
-        echo '商品価格を整数で入力してください。';
-    } else
-    if (empty($_POST['size'])) {
-        echo '商品サイズを入力してください。';
-    } else
-    if (empty($_POST['color'])) {
-        echo '商品色を入力してください。';
+        echo '概要を入力してください。';
     } else
     if (empty($_POST['category'])) {
         echo 'カテゴリーを入力してください。';
     } else
-    if (empty($_POST['sozai'])) {
-        echo '商品素材を入力してください。';
+    if (empty($_POST['status'])) {
+        echo '連載状況を入力してください。';
     } else
-    if (!preg_match('/[0-9]+/', $_POST['zaiko'])) {
-        echo '商品在庫を整数で入力してください。';
+    if (empty($_POST['read_pro'])) {
+        echo '商品色を入力してください。';
     } else
-    if (empty($_POST['image'])) {
-        echo 'パスを入力してください。';
-    }
-    if($sql->execute([$_POST['name'],$_POST['ex'], $_POST['price'],$_POST['size'],$_POST['color'],$_POST['category'],$_POST['sozai'],$_POST['zaiko'],$_POST['image'], $_POST['id']])){
+    if (empty($_POST['author'])) {
+        echo 'を入力してください。';
+    } else
+    if (empty($_POST['artist'])) {
+        echo 'を入力してください。';
+    } else
+    if (is_uploaded_file($_FILES['image']['tmp_name'])){
+        if(!file_exists('upload')){
+            mkdir('upload');
+        }
+        $file='upload/'.basename($_FILES['image']['name'])
+    } 
+
+    if($sql->execute([$_POST['name'],$_POST['ex'], $_POST['category'],$_POST['status'],$_POST['read_pro'],$_POST['author'],$_POST['artist'],$_POST['image'], $_POST['id']])){
         echo '<font color="red">更新に成功しました。</font>';
     }else{
         echo '<font color="red">更新に失敗しました。</font>';
@@ -51,7 +51,7 @@ $pdo=new PDO($connect, USER, PASS);
         <hr>
 <?php
 
-        echo '<p><a href="ad-product.php"><img src="./image/undo.png" alt="Detail" width="30" height="30"></a></p>';
+        echo '<p><a href="mylist.php"><img src="./image/undo.png" alt="Detail" width="30" height="30"></a></p>';
    
 ?>
 </div>
--- src/edit.php
+++ src/edit.php
@@ -3,56 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>商品情報変更</title>
+    <title>情報変更</title>
     <link rel="stylesheet" href="css/detail.css">
 </head>
 <body>
-        <?php require 'header.php'; ?>
         <?php require 'db-connect.php'; ?>
         <div class="container">
 
         <?php
         $pdo=new PDO($connect, USER, PASS);
-        $sql=$pdo->prepare('select * from product where id=?');
+        $sql=$pdo->prepare('select * from mylist where id=?');
         $sql->execute([$_GET['id']]);
         foreach ($sql as $row){
-            echo '<form action="pro-edit-output.php" method="post" enctype="multipart/form-data">';
+            echo '<form action="edit-output.php" method="post" enctype="multipart/form-data">';
 echo '<table>';
-echo '<tr><td>商品番号</td>';
-echo '<td><input type="text" name="id" value="', $row['id'] , '">';
-echo '</td></tr>';
-echo '<tr><td>商品名</td>';
+echo '<tr><td>漫画名</td>';
 echo '<td><input type="text" name="name" value="', $row['name'] , '">';
 echo '</td></tr>';
-echo '<tr><td>商品説明</td>';
+echo '<tr><td>概要  </td>';
 echo '<td><input type="text" name="ex" value="', $row['ex'] , '">';
-echo '</td></tr>';
-echo '<tr><td>価格</td>';
-echo '<td><input type="text" name="price" value="', $row['price'] , '">';
-echo '</td></tr>';
-echo '<tr><td>サイズ</td>';
-echo '<td><input type="text" name="size" value="', $row['size'] , '">';
-echo '</td></tr>';
-echo '<tr><td>色</td>';
-echo '<td><input type="text" name="color" value="', $row['color'] , '">';
 echo '</td></tr>';
 echo '<tr><td>カテゴリー</td>';
 echo '<td><input type="text" name="category" value="', $row['category'] , '">';
 echo '</td></tr>';
-echo '<tr><td>素材</td>';
-echo '<td><input type="text" name="sozai" value="', $row['sozai'] , '">';
+echo '<tr><td>連載状況</td>';
+echo '<td><input type="text" name="status" value="', $row['status'] , '">';
 echo '</td></tr>';
-echo '<tr><td>在庫</td>';
-echo '<td><input type="text" name="zaiko" value="', $row['zaiko'] , '">';
+echo '<tr><td>読書状況</td>';
+echo '<td><input type="text" name="read_pro" value="', $row['read_pro'] , '">';
+echo '</td></tr>';
+echo '<tr><td>作者</td>';
+echo '<td><input type="text" name="author" value="', $row['author'] , '">';
+echo '</td></tr>';
+echo '<tr><td>イラストレーター</td>';
+echo '<td><input type="text" name="artist" value="', $row['artist'] , '">';
 echo '</td></tr>';
 echo '<tr><td>商品画像</td>';
-echo '<td><input type="text" name="image" value="', $row['image'] , '">';
+echo '<td><input type="file" name="image" value="', $row['image'] , '">';
 echo '</td></tr>';
 
 echo '</table>';
 echo '<input type="submit" value="変更">';
 echo '</form>';
-echo '<p><a href="ad-product.php"><img src="./image/undo.png" alt="Detail" width="30" height="30"></a></p>';
+echo '<p><a href="mylist.php"><img src="./image/undo.png" alt="Detail" width="30" height="30"></a></p>';
         }
 ?>
 </body>
